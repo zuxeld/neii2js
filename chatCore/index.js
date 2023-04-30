0a3d2889b014923898bc75fe0804248eaf2943ad
zuxeld <zuxeld@gmail.com>
2023-04-30 16:58:29 +0400

    feat: part of chat core

--- chatCore/index.js
+++ chatCore/index.js
@@ -1,17 +1,38 @@
-let chatMemory = {};
+let chatMemory = {
+    mem: [],
+
+};
+function isDeleteRuleCommand(words) {
+    
+}
+function isAddRuleCommand(words) {
+    
+}
 export default {
-    enterMassage(userMassage) {
-
-    },
-    getChatText() {
-
-    },
-
-
+    // rebild memory from chat history:
     rebildChatMemory(chatHistory) {
 
     },
     getChatHistory() {
+
+    },
+
+    enterMassage(userMassage) {
+        chatMemory.mem += 'hoz: ' + userMassage;
+        let words = userMassage.split(' ');
+        words = words.filter(function(item, index, array) {
+            if (item === '') return false;
+            return true;
+        });
+        if (isDeleteRuleCommand(words)) {
+            
+        } else if (isAddRuleCommand(words)) {
+            
+        } else {
+            
+        }
+    },
+    getChatText() {
 
     },
 }
